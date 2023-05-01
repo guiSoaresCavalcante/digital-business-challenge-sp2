f2c606f9dc551e209ba6502d66525ed58928a3c3
Guilherme <gui.cavalcante1999@outlook.com.br>
2023-05-01 18:38:32 -0300

    add: adicionando as entidades

--- api-plusoft/src/main/java/br/com/plusoft/ApiPlusoftApplication.java
+++ api-plusoft/src/main/java/br/com/plusoft/ApiPlusoftApplication.java
@@ -1,4 +1,4 @@
-package br.com.plusoft.apiplusoft;
+package br.com.plusoft;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
