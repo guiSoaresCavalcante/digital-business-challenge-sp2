c1542f531e7abbd090d977b1534fc9675f79a754
Guilherme <gui.cavalcante1999@outlook.com.br>
2023-05-01 18:53:37 -0300

    add: adicionando as classes repositories

---
+++ api-plusoft/src/main/java/br/com/plusoft/repositories/SoftwareGestaoRepository.java
@@ -0,0 +1,7 @@
+package br.com.plusoft.repositories;
+
+import br.com.plusoft.entities.SoftwareGestaoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareGestaoRepository extends JpaRepository<SoftwareGestaoEntity, Long> {
+}
